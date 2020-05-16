499e37885e9835ae325e8106757bd51b0b019a5d
FarhadNawab <farhadnawab.official@gmail.com>
2020-05-17 03:50:32 +0500

    Navigation responsive.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 AOS.init();
+
+/** Nav Toggle **/
+function openMenu(){
+  var navMenuDiv = document.getElementById("nav-content");
+  navMenuDiv.classList.add("active");
+}
+function closeMenu(){
+  var navMenuDiv = document.getElementById("nav-content");
+  navMenuDiv.classList.remove("active");
+}
