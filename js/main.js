1e9b0c1e5a73dac9b07dc7be2e320da2c1dd6e1a
FarhadNawab <farhadnawab.official@gmail.com>
2020-05-26 01:56:35 +0500

    Stories Page Completed Destkop / Tablet / Mobile

--- js/main.js
+++ js/main.js
@@ -9,3 +9,20 @@ function closeMenu(){
   var navMenuDiv = document.getElementById("nav-content");
   navMenuDiv.classList.remove("active");
 }
+
+/*** POPUP ****/
+$(document).on('click', '[data-popup]', function(e) {// open popup
+  $popup = $("#"+ $(this).attr("data-popup"));
+
+  $("body").addClass("popup-active");
+  $popup.addClass("active");
+});
+$(document).on('click', '.popup', function(e) {// close popup
+
+  popupActiveId = $(".popup.active").attr("id");
+  
+  if(e.target.id == popupActiveId){//if clicked outside content area
+      $("body").removeClass("popup-active");
+      $(".popup").removeClass("active");
+  }
+});
