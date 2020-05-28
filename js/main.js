89ac7be98ed42f67a23473a4fee78efb7a92cf70
FarhadNawab <farhadnawab.official@gmail.com>
2020-05-29 02:56:13 +0500

    Accountability Page Completed Desktop / Tablet / Mobile

--- js/main.js
+++ js/main.js
@@ -26,3 +26,37 @@ $(document).on('click', '.popup', function(e) {// close popup
       $(".popup").removeClass("active");
   }
 });
+
+/*** Open Book ***/
+$(document).on('click', '.open-book .arrow-left', function(e) {// open popup
+  $activeElem = $(this).closest(".open-book").find(".pages > .page.active");
+  if($activeElem.prev().length > 0){
+    $activeElem.removeClass("active");
+    $activeElem.prev().addClass("active");
+  }
+});
+
+$(document).on('click', '.open-book .arrow-right', function(e) {// open popup
+  $activeElem = $(this).closest(".open-book").find(".pages > .page.active");
+  if($activeElem.next().length > 0){
+    $activeElem.removeClass("active");
+    $activeElem.next().addClass("active");
+  }
+});
+
+
+/*** Tabs ***/
+$(document).on('click', '.tab .tab-buttons > a', function(e) {// open popup
+  $currentTab = $(this).closest(".tab");
+  $activeElem = $currentTab.find("#"+ $(this).attr("data-target"));
+  if($activeElem.length > 0 && !$activeElem.hasClass("active")){
+    
+    //Tab button active
+    $currentTab.find(".tab-buttons > a").removeClass("active");
+    $(this).addClass("active");
+
+    //Tab content Active
+    $currentTab.find(".tab-content").removeClass("active");
+    $activeElem.addClass("active");
+  }
+});
